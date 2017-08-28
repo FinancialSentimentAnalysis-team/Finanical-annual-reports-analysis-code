15f15b1972a92d208101bd686c3e1dee80fa93b3
罗旺 <13353830165@163.com>
2017-08-28 15:18:38 +0800

    Update pdf2txt.py

--- luowang/Data_Processing/pdf2txt.py
+++ luowang/Data_Processing/pdf2txt.py
@@ -17,38 +17,34 @@ import os
 def pdfTotxt1(pdf_path, txt_path):
     '''
     function: get the text according to the structure of pdf page
-    pdf_path: the target path which is used to save translated pdf file 
-    txt_path: the source file path which are ready for being translated into pdf file
+    pdf_path: the source pdf path 
+    txt_path: the target txt file path
     '''
-    '''
-    if os.path.exists(txt_path):
+    
+    ### check whether the source pdf file is exist ##########
+    if not os.path.exists(pdf_path):
         return
-    '''
-    #content=''
-    #import os
-    #os.chdir(r'F:\test')
+     
+    ### check whether the target txt path is exist ##########
+    if not os.path.exists(txt_path):
+        os.path.mkdir(txt_path)
+        
+    
+   
     fp = open(pdf_path, 'rb')
-    #À´´´½¨Ò»¸öpdfÎÄµµ·ÖÎöÆ÷
     parser = PDFParser(fp)  
-    #´´½¨Ò»¸öPDFÎÄµµ¶ÔÏó´æ´¢ÎÄµµ½á¹¹
     document = PDFDocument(parser)
-    # ¼ì²éÎÄ¼þÊÇ·ñÔÊÐíÎÄ±¾ÌáÈ¡
-    if not document.is_extractable:
+    if not document.is_extractable:     ### check whether the pdf is protected and not be allowed to extract 
         raise PDFTextExtractionNotAllowed
     else:
-        # ´´½¨Ò»¸öPDF×ÊÔ´¹ÜÀíÆ÷¶ÔÏóÀ´´æ´¢¹²ÉÍ×ÊÔ´
+        
         rsrcmgr=PDFResourceManager()
-        # Éè¶¨²ÎÊý½øÐÐ·ÖÎö
         laparams=LAParams()
-        # ´´½¨Ò»¸öPDFÉè±¸¶ÔÏó
         # device=PDFDevice(rsrcmgr)
         device=PDFPageAggregator(rsrcmgr,laparams=laparams)
-        # ´´½¨Ò»¸öPDF½âÊÍÆ÷¶ÔÏó
         interpreter=PDFPageInterpreter(rsrcmgr,device)
-        # ´¦ÀíÃ¿Ò»Ò³
         for page in PDFPage.create_pages(document):
             interpreter.process_page(page)
-            # ½ÓÊÜ¸ÃÒ³ÃæµÄLTPage¶ÔÏó
             layout=device.get_result()
             for x in layout:
                 #print type(x)
@@ -66,11 +62,20 @@ def pdfTotxt1(pdf_path, txt_path):
 
 def pdfTotxt2(pdf_path, txt_path) :
     '''
-    if os.path.exists(txt_path):
-        return
+    function: it is a violent method to translate pdf into txt when the pdf file is not allowed to be extracted
+    pdf_path: the source pdf path 
+    txt_path: the target txt file path
     '''
+  
+    ### check whether the source pdf file is exist ##########
+    if not os.path.exists(pdf_path):
+        return
+     
+    ### check whether the target txt path is exist ##########
+    if not os.path.exists(txt_path):
+        os.path.mkdir(txt_path)
+      
     content =''
-    #Êä³öÎÄ¼þÃû£¬ÕâÀïÖ»´¦Àíµ¥ÎÄµµ£¬ËùÒÔÖ»ÓÃÁËargv£Û1£Ý
     try:
         outfile = txt_path
         args =[pdf_path]
@@ -80,7 +85,7 @@ def pdfTotxt2(pdf_path, txt_path) :
         password = ''
         maxpages = 0
         rotation = 0
-        codec = 'utf-8'   #Êä³ö±àÂë
+        codec = 'utf-8'   
 
         caching = True
         imagewriter = None
@@ -91,12 +96,12 @@ def pdfTotxt2(pdf_path, txt_path) :
 
         rsrcmgr = PDFResourceManager(caching=caching)
         outfp = file(outfile,'a')
-        #pdf×ª»»
+       
         device = TextConverter(rsrcmgr, outfp, codec=codec, laparams=laparams, imagewriter=imagewriter)
         for fname in args:
             fp = file(fname,'rb')
             interpreter = PDFPageInterpreter(rsrcmgr, device)
-            #´¦ÀíÎÄµµ¶ÔÏóÖÐÃ¿Ò»Ò³µÄÄÚÈÝ
+            
             for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching, check_extractable=False) :
                 page.rotate = (page.rotate+rotation) % 360
                 interpreter.process_page(page)
